42d2e771173c236000611e09adc9f915583fcc0c
Ismail El Shinnawy <ismailelshennawy@gmail.com>
2023-05-18 01:21:49 +0300

    chore(styles): add css-reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import {Layout} from '@/components'
 import { AppProps } from 'next/app';
+
+import "../styles/css-reset.css"
  
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
